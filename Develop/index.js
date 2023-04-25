cabd6c090c2fa9702b55333a02a311221e4e9090
Emma Carr <lorenagracex@gmail.com>
2023-04-25 17:13:36 -0500

    license badge

--- Develop/index.js
+++ Develop/index.js
@@ -5,51 +5,77 @@ const inquirer = require("inquirer");
 // validate is used to keep the user from skipping the question
 const questions = [
   {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?',
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
     validate: (answer) => {
-      if (answer === '') {
+      if (answer === "") {
         return "Answer required, try again";
       }
       return true;
     },
   },
-  
+
   {
-    type: 'input',
-    name: 'description',
-    message: 'Enter a brief description of your project (Why did you make it? What does it solve? Key take away?)',
+    type: "input",
+    name: "description",
+    message:
+      "Enter a brief description of your project (Why did you make it? What does it solve? Key take away?)",
     validate: (answer) => {
-      if (answer === '') {
-        return "Answer required, try again";
-      }
-      return true;
-    },
-  },
-  {
-    type: 'input',
-    name: 'installation',
-    message: 'Enter step by step instructions to use your application. If no installation is required please provide a link to your deployed application.',
-    validate: (answer) => {
-      if (answer === '') {
+      if (answer === "") {
         return "Answer required, try again";
       }
       return true;
     },
   },
   {
-    type: 'input',
-    name: 'usage',
-    message: 'Enter detailed instructions on how to use the application. Provide examples if possible.',
+    type: "input",
+    name: "installation",
+    message:
+      "Enter step by step instructions to use your application. If no installation is required please provide a link to your deployed application.",
     validate: (answer) => {
-      if (answer === '') {
+      if (answer === "") {
         return "Answer required, try again";
       }
       return true;
     },
   },
-  
+  {
+    type: "input",
+    name: "usage",
+    message:
+      "Enter detailed instructions on how to use the application. Provide examples if possible.",
+    validate: (answer) => {
+      if (answer === "") {
+        return "Answer required, try again";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "Enter any collaborators, resources, etc.",
+    validate: (answer) => {
+      if (answer === "") {
+        return "Answer required, try again";
+      }
+      return true;
+    },
+  },
+  {
+    type: "list",
+    name: "license",
+    message:
+      "Choose the license you want, use your arrow keys to navigate through the choices.",
+    choices: [
+      {
+        name: "Apache 2.0",
+        value:
+          "[![License](https://img.shields.io/badge/License-Apache%202.0-7EA8BE)](https://opensource.org/licenses/Apache-2.0)",
+      },
+    ],
+  },
 ];
 
 // TODO: Create a function to write README file
